9ceb4cfb1eef6a46719f8965cadc7be318aeb7e7
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-05-28 01:04:28 +0530

    restructure & rename

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'babel-polyfill';
 import 'tachyons';
 import {BrowserRouter} from 'react-router-dom';
 
